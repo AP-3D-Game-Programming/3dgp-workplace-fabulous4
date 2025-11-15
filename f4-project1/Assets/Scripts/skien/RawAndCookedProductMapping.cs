cf771ede2e0dc7496c5c36d1a1d1c852da9e7a42
KikyR <kean.rottiers@gmail.com>
2025-11-15 16:15:39 +0100

    Finished oven with progress bar

--- f4-project1/Assets/Scripts/skien/RawAndCookedProductMapping.cs
+++ f4-project1/Assets/Scripts/skien/RawAndCookedProductMapping.cs
@@ -6,5 +6,6 @@ using UnityEngine;
         public string RawProductName;
         public GameObject RawProductPrefab;
         public GameObject CookedProductPrefab;
+        public int BakeTime;
     }
     
--- f4-project1/Assets/Scripts/skien/ovenBakeScript.cs
+++ f4-project1/Assets/Scripts/skien/ovenBakeScript.cs
@@ -1,32 +1,94 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class ovenBakeScript : MonoBehaviour
 {
+
+    public Image progressBarFillImage;
+    public GameObject progressBarCanvas;
+    private float currentBakeTime = 0f;
     public GameObject parentForProductSpawn;
-    public RawCookedProductDataSO rawCookedProductData; //product mapping in de unity editor waar producten en ingredienten kunnen toegevoegd worden.
+    public RawCookedProductDataSO rawCookedProductData;
+    private bool isBaking = false; 
+    private GameObject currentRawProduct = null; 
 
-
-    private void OnTriggerStay(Collider other) //checkt of er een ingredient in de sphere collider van de oven zit, E klik voegt dit dan toe aan de huidige items in de mixer. 
+    private void OnTriggerStay(Collider other)
     {
+        if (isBaking) 
+            return; 
 
         string objectInSphereColliderName = other.name;
+
         foreach (RawCookedProductMapping mapping in rawCookedProductData.Mappings)
         {
             if (other.name.StartsWith(mapping.RawProductName) && Input.GetKeyDown(KeyCode.T))
             {
-                Debug.Log($"{objectInSphereColliderName} baked to cooked product");
-                Destroy(other.gameObject);
-                Vector3 localOffset = new Vector3(-0.5f, 1f, 1f);
-                Vector3 worldSpawnPosition = parentForProductSpawn.transform.position + localOffset;
-                GameObject newProduct = Instantiate(
-                    mapping.CookedProductPrefab,
-                    worldSpawnPosition, // Gebruik de berekende wereldpositie
-                    Quaternion.identity,
-                    parentForProductSpawn.transform // De parent is optioneel, maar kan behouden blijven als je dat wilt.
-                );
+                StartBaking(other.gameObject, mapping); 
                 return;
             }
         }
     }
-}
+    
+    public void StartBaking(GameObject rawProduct, RawCookedProductMapping mapping)
+    {
+        currentRawProduct = rawProduct;
+        rawProduct.SetActive(false);
+        isBaking = true;
+        
+        if (progressBarCanvas != null)
+        {
+            progressBarCanvas.SetActive(true);
+        }
 
+        if (progressBarFillImage != null)
+        {
+            progressBarFillImage.fillAmount = 1f;
+        }
+
+        Debug.Log($"Start bakken van {rawProduct.name}. Duur: {mapping.BakeTime} seconden.");
+
+        StartCoroutine(BakeTimer(mapping));
+    }
+
+    IEnumerator BakeTimer(RawCookedProductMapping mapping)
+    {
+
+    float elapsedTime = 0f;
+    float totalBakeTime = mapping.BakeTime;
+
+    while (elapsedTime < totalBakeTime)
+    {
+        elapsedTime += Time.deltaTime;
+        
+        float progress = 1f - (elapsedTime / totalBakeTime);
+        
+        if (progressBarFillImage != null)
+        {
+            progressBarFillImage.fillAmount = progress;
+        }
+
+        yield return null;
+    }
+    
+    if (progressBarCanvas != null)
+    {
+        progressBarCanvas.SetActive(false); 
+    }
+
+
+    Debug.Log($"Bakken voltooid! Spawnen van {mapping.CookedProductPrefab.name}");
+    Destroy(currentRawProduct);
+    Vector3 localOffset = new Vector3(0f, 1f, -1f);
+    Vector3 worldSpawnPosition = parentForProductSpawn.transform.position + localOffset;
+    Instantiate(
+        mapping.CookedProductPrefab,
+        worldSpawnPosition,
+        Quaternion.identity,
+        parentForProductSpawn.transform
+    );
+
+    currentRawProduct = null;
+    isBaking = false;
+    }
+}
